70ec583853d39ad7d5e91e6f9639131db781cbc5
Isabella Shao <issmyyyy@link.cuhk.edu.hk>
2019-05-16 13:44:08 +0200

    updated hashtags

--- server.R
+++ server.R
@@ -9,7 +9,7 @@ library(syuzhet)
 library(plotly)
 
 shinyServer(function(input, output, session) {
-  # Create object for reactive value
+  # Create reactive object
   store_value <- reactiveValues(value = character())
   
   # Update when input changes
@@ -18,8 +18,8 @@ shinyServer(function(input, output, session) {
     # Store user input into reactive object
     store_value$value <- input$hashtag
     
-    # When user input is "Game of Throne"
-    if (store_value$value == "Game of Thrones") {
+    # When user input is "Brexit"
+    if (store_value$value == "Brexit") {
       # Authonitical keys
       consumer_key <- 'wU5aIE1E6Mu3JOSkrlWLYiHLP'
       consumer_secret <- 'EajqHPqtlJHbu0MmdS87yfAhL7coU15duID7SDiEVJwL5rt2RR'
@@ -28,10 +28,10 @@ shinyServer(function(input, output, session) {
       setup_twitter_oauth(consumer_key,consumer_secret,access_token, access_secret)
       
       # Search and store tweets
-      tweets = searchTwitter("#gameofthrones", n = 150, lang = "en")
+      tweets = searchTwitter("#brexit", n = 200, lang = "en")
       tweets.df = twListToDF(tweets)
       
-      # Clean tweets
+      # Remove characters that are not english words
       tweets.df$text=gsub("&amp", "", tweets.df$text)
       tweets.df$text = gsub("&amp", "", tweets.df$text)
       tweets.df$text = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", tweets.df$text)
@@ -43,18 +43,18 @@ shinyServer(function(input, output, session) {
       tweets.df$text = gsub("^\\s+|\\s+$", "", tweets.df$text)
       tweets.df$text <- iconv(tweets.df$text, "UTF-8", "ASCII", sub="")
       
-      # Emotions for each tweet using NRC dictionary
+      # Sentiment analysis using NRC dictionary
       emotions <- get_nrc_sentiment(tweets.df$text)
       emo_bar = colSums(emotions)
       emo_sum = data.frame(count=emo_bar, emotion=names(emo_bar))
       emo_sum$emotion = factor(emo_sum$emotion, levels=emo_sum$emotion[order(emo_sum$count, decreasing = TRUE)])
       
       # Print text
-      output$text <- renderText({paste("How do people feel about the final season of GOT?")})
+      output$text <- renderText({paste("How do people feel about brexit?")})
     }
     
-    # When user input is "Avengers: End Game"
-    else if (store_value$value == "Avengers: End Game") {
+    # When user input is "Samsung fold"
+    else if (store_value$value == "Samsung fold") {
       # Authonitical keys
       consumer_key <- 'wU5aIE1E6Mu3JOSkrlWLYiHLP'
       consumer_secret <- 'EajqHPqtlJHbu0MmdS87yfAhL7coU15duID7SDiEVJwL5rt2RR'
@@ -63,10 +63,10 @@ shinyServer(function(input, output, session) {
       setup_twitter_oauth(consumer_key,consumer_secret,access_token, access_secret)
       
       # Search and store tweets
-      tweets = searchTwitter("#endgame", n = 150, lang = "en")
+      tweets = searchTwitter("#samsungfold", n = 200, lang = "en")
       tweets.df = twListToDF(tweets)
       
-      # Clean tweets
+      # Remove characters that are not english words
       tweets.df$text=gsub("&amp", "", tweets.df$text)
       tweets.df$text = gsub("&amp", "", tweets.df$text)
       tweets.df$text = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", tweets.df$text)
@@ -78,18 +78,18 @@ shinyServer(function(input, output, session) {
       tweets.df$text = gsub("^\\s+|\\s+$", "", tweets.df$text)
       tweets.df$text <- iconv(tweets.df$text, "UTF-8", "ASCII", sub="")
       
-      # Emotions for each tweet using NRC dictionary
+      # Sentiment analysis using NRC dictionary
       emotions <- get_nrc_sentiment(tweets.df$text)
       emo_bar = colSums(emotions)
       emo_sum = data.frame(count=emo_bar, emotion=names(emo_bar))
       emo_sum$emotion = factor(emo_sum$emotion, levels=emo_sum$emotion[order(emo_sum$count, decreasing = TRUE)])
       
       # Print text
-      output$text <- renderText({paste("How do people feel about the final Avengers movie?")})
+      output$text <- renderText({paste("How do people feel about the new product Samsung fold?")})
     }
     
-    # When user input is "James Charles"
-    else if (store_value$value == "James Charles") {
+    # When user input is "Credit Suisse"
+    else if (store_value$value == "Credit Suisse") {
       # Authonitical keys
       consumer_key <- 'wU5aIE1E6Mu3JOSkrlWLYiHLP'
       consumer_secret <- 'EajqHPqtlJHbu0MmdS87yfAhL7coU15duID7SDiEVJwL5rt2RR'
@@ -98,10 +98,10 @@ shinyServer(function(input, output, session) {
       setup_twitter_oauth(consumer_key,consumer_secret,access_token, access_secret)
       
       # Search and store tweets
-      tweets = searchTwitter("#jamescharles", n = 150, lang = "en")
+      tweets = searchTwitter("#creditsuisse", n = 200, lang = "en")
       tweets.df = twListToDF(tweets)
       
-      # Clean tweets
+      # Remove characters that are not english words
       tweets.df$text=gsub("&amp", "", tweets.df$text)
       tweets.df$text = gsub("&amp", "", tweets.df$text)
       tweets.df$text = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", tweets.df$text)
@@ -113,14 +113,14 @@ shinyServer(function(input, output, session) {
       tweets.df$text = gsub("^\\s+|\\s+$", "", tweets.df$text)
       tweets.df$text <- iconv(tweets.df$text, "UTF-8", "ASCII", sub="")
       
-      # Emotions for each tweet using NRC dictionary
+      # Sentiment analysis using NRC dictionary
       emotions <- get_nrc_sentiment(tweets.df$text)
       emo_bar = colSums(emotions)
       emo_sum = data.frame(count=emo_bar, emotion=names(emo_bar))
       emo_sum$emotion = factor(emo_sum$emotion, levels=emo_sum$emotion[order(emo_sum$count, decreasing = TRUE)])
       
       # Print text
-      output$text <- renderText({paste("How do people feel about the scandalous James Charles?")})
+      output$text <- renderText({paste("How do people feel about Credit Suisse?")})
     }
     
     # Generate histogram
--- ui.R
+++ ui.R
@@ -1,20 +1,19 @@
 library(shiny)
 library(plotly)
 shinyUI(fluidPage(
-  
   headerPanel("Twitter Sentiment Analysis"),
   textOutput("time"),
   
   # Getting User Inputs
   sidebarPanel(
-    selectInput("hashtag" , "Choose a trending topic to analyse!", c("Game of Thrones", "Avengers: End Game", "James Charles")),
+    selectInput("hashtag" , "Choose a trending topic to analyse!", c("Brexit", "Samsung fold" , "Credit Suisse")),
     textOutput("text")
   ),
   
   mainPanel(
-    
     tabsetPanel(
-      
       tabPanel("Histogram", HTML("<div><h4>Emotion types of your selected hashtag<h4></div>"), plotlyOutput("plot"), HTML("<div>The graph takes minutes to generate, please be patient...</div>"))
-  
-))))
+      )
+    )
+  )
+)
